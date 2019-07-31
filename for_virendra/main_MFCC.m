bbb39080b3d40147c4cf68e1110ea85ce1e3e7fa
Puneet Bawa <bawapuneet994@gmail.com>
2019-07-31 18:59:43 +0530

    Add files via upload
    Final code of MFCC is in folder for_virendra

---
+++ for_virendra/main_MFCC.m
@@ -0,0 +1,57 @@
+%% Implement HRead in matlab version
+clc; clear all; close all;
+
+fid=fopen('C:\Users\Admin\Downloads\for_virendra\for_virendra\in_wav_file.txt','r');
+fid_mfcc=fopen('C:\Users\Admin\Downloads\for_virendra\for_virendra\out_mfcc_file.txt','r');
+fid_prof=fopen('C:\Users\Admin\Downloads\for_virendra\for_virendra\pros_feats_file.txt','r');
+
+Tw = 25;                % analysis frame duration (ms)
+Ts = 10;                % analysis frame shift (ms)
+alpha = 0.97;           % preemphasis coefficient
+M = 20;                 % number of filterbank channels 
+C = 12;                 % number of cepstral coefficients
+L = 22;                 % cepstral sine lifter parameter
+LF = 300;               % lower frequency limit (Hz)
+HF = 3700;              % upper frequency limit (Hz)
+
+%value of speech samples as multiple postive and negative numbers 
+%fs=4410in
+%nbits=16
+%fid = fopen('speech_matrix.txt','w');
+%fprintf(fid, '%s\n', speech);
+%fclose(fid);
+
+
+for i=1:2175;
+   
+   tline=fgetl(fid);
+   tline1=fgetl(fid_mfcc);
+   tline2=fgetl(fid_prof);
+   [ado,fs]=audioread(tline);
+   [ MFCCs, FBEs, frames ] = MFCC( ado, fs, Tw, Ts, alpha, @hamming, [LF HF], M, C+1, L );
+                %mfcc(sample matrix from audio file, fs=44100, Tw=25,
+                %Ts=10ms, alpha=0.97, hamming?, [300 3700], M=20, C+1=13, L=22
+    % Generate data needed for plotting 
+   [ Nw, NF ] = size( frames );
+   disp(NF);
+   
+   if ~ischar(tline),break,end;
+   %[ado,fs]=audioread(tline);
+   %[Nw,Nf]=size(fs);
+   %ado=ado(:,1);
+   %ado=ado./max(abs(ado));
+   %[ml,f]=MFCC(ado,fs,'z0Mp',12,23,20e-3*fs,10e-3*fs,0,0.5,0.97);
+   %zeroth_ml=ml(:,1);ml=ml(:,2:13); t=1:1:12;T=repmat(1*(t),size(ml,1),1);ml=T.*ml;ml=[12*zeroth_ml ml];
+   PROm = dlmread(tline2);
+   %disp(PROm);
+   Pros = PROm(1:NF,[2,3,4,5]);
+   disp(Pros);
+   TMFCCs = transpose(MFCCs);
+   MFPro = [TMFCCs Pros];
+   TMFPro = transpose(MFPro);
+   writehtk(tline1,TMFPro,0.010,8198);
+end
+
+fclose(fid);
+fclose(fid_mfcc);
+fclose(fid_prof);
